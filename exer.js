29d1588877be44d0f7ee18bc571f2a9a1cbbb8b8
reneeArguelles <mrd.arguelles@gmail.com>
2022-09-20 11:10:02 +0800

    replace the .map method with a function - add other ways to add element to an array - add another way to determine truthy and falsy (git add . method)

--- exer.js
+++ exer.js
@@ -1,5 +1,16 @@
 const sampleArray = [1, 0, null, undefined, false, -0, 0n, "", '', ``, NaN, 'A', 123, -123, -0x0, -0.0, { name: "ObjectName" }, 0, '0', 'false', function(){}, BigInt(1232424), BigInt("0b11111111111111111111111111111111111111111111111111111")];
 
-const checkTruthyOrFalsy = sampleArray.map((element) => Boolean(element));
+function checkTruthyFalsy(array){
+    let newResult = [];
+    // for(let index = 0; index < array.length; index++){
+    //     //newResult.push(Boolean(array[index]));
+    //     newResult = [...newResult, !!array[index]];
+    // }
 
-console.log(checkTruthyOrFalsy);
+    array.forEach((element) => {
+        newResult = [...newResult, !!element];
+    });
+    return newResult;
+};
+
+console.log(checkTruthyFalsy(sampleArray));
